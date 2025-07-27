7f4dc8bb1ce5ba03d8537b0e0afb9ed8564948d8
Gerson-Santiago <gersondossantossantiago@gmail.com>
2025-07-27 18:02:20 -0300

    fix: adiciona import defineConfig no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 // vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
 export default defineConfig({
   base: '/',
   plugins: [react()],
