fc7dee188bd036dc9a4da26b48a5b744fea61e13
Gerson Santiago <gersondossantossantiago@gmail.com>
2025-08-19 16:26:10 -0300

    Build de produção: base ajustado para /seduc/ e pasta dist pronta

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/sorteador-online/', 
+  base: '/seduc/', 
   plugins: [react()],
   resolve: {
     alias: {
